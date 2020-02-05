dddf1f4fc35ca0cf19efd30b8a87ce106bf711c1
Gabriel Dean Olivar <gd.olivar@gmail.com>
2020-02-05 14:30:48 +0000

    HLP Content Change

--- src/pages/Work/HelloLampPost.js
+++ src/pages/Work/HelloLampPost.js
@@ -31,7 +31,7 @@ const GBKCareers = () => (
 
     <div className="container">
         <div className="background-text">
-            <p>GBK's careers website aims to tell the story about the humble begginings of the chain while also listing the available positions throughout all of their restaurants.</p>
+            <p>Hello Lamp Post aims to improve public engagement with clients working with the public and private sector. A website is an integral part of conveying information to potential clients.</p>
         </div>
 
         <div className="project-details">
